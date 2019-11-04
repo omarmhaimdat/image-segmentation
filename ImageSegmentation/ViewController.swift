6ae8ea40a118c6fb2a3c1bdba9d1300727696e41
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2019-11-04 16:19:59 +0100

    Initial Commit

---
+++ ImageSegmentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageSegmentation
+//
+//  Created by M'haimdat omar on 04-11-2019.
+//  Copyright © 2019 M'haimdat omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
